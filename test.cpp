e0ede15b036816d7a8fb0810479b482d640b65a8
erobert <etiennerobert33@gmail.com>
2020-06-10 22:04:35 +0200

    add tests

--- test.cpp
+++ test.cpp
@@ -3,6 +3,8 @@
 #include "testXOString.h"
 
 using std::string;
+using std::to_string;
+using std::stringstream;
 
 class TestXOStringTest : public CppUnit::TestCase
 {
@@ -11,31 +13,44 @@ class TestXOStringTest : public CppUnit::TestCase
 
   void runTest()
   {
+    // Test with string ""
     CPPUNIT_ASSERT(testXOString(0, 0, ""));
     CPPUNIT_ASSERT(!testXOString(1, 0, ""));
     CPPUNIT_ASSERT(!testXOString(0, 1, ""));
     CPPUNIT_ASSERT(!testXOString(1, 1, ""));
     CPPUNIT_ASSERT(!testXOString(100000, 0, ""));
 
+    // Test with one X
     CPPUNIT_ASSERT(testXOString(1, 0, "X"));
     CPPUNIT_ASSERT(!testXOString(0, 0, "X"));
     CPPUNIT_ASSERT(!testXOString(0, 1, "X"));
     CPPUNIT_ASSERT(!testXOString(1, 1, "X"));
 
+    // Test with incorrect character
     CPPUNIT_ASSERT(!testXOString(0, 0, "V"));
     CPPUNIT_ASSERT(!testXOString(1, 0, "V"));
     CPPUNIT_ASSERT(!testXOString(0, 1, "V"));
 
+    // Test with multiple lenghts of X
     CPPUNIT_ASSERT(testXOString(3, 0, "XXX"));
     CPPUNIT_ASSERT(!testXOString(4, 0, "XXXX"));
     CPPUNIT_ASSERT(!testXOString(5, 0, "XXXXX"));
 
+    // Tests from subject
     CPPUNIT_ASSERT(testXOString(2, 3, "XOOOX"));
     CPPUNIT_ASSERT(testXOString(2, 3, "OXOXO"));
     CPPUNIT_ASSERT(testXOString(6, 1, "XXXOXXX"));
     CPPUNIT_ASSERT(!testXOString(6, 1, "XXOXXXX"));
   }
 };
+
+void testGenerate(int countX, int countO)
+{
+  string s = generateXOString(countX, countO);
+  stringstream errorss;
+  errorss << "countX: " << countX << "; countY: " << countO << "; s: " << s;
+  CPPUNIT_ASSERT_MESSAGE(errorss.str(), testXOString(countX, countO, s));
+}
 
 class GenerateXOStringTest : public CppUnit::TestCase
 {
@@ -44,7 +59,17 @@ class GenerateXOStringTest : public CppUnit::TestCase
 
     void runTest()
     {
-      CPPUNIT_ASSERT(testXOString(0, 0, generateXOString(0, 0)));
+      testGenerate(0, 0);
+      testGenerate(1, 0);
+      testGenerate(0, 1);
+      testGenerate(1, 1);
+
+      testGenerate(4, 1);
+      testGenerate(1, 4);
+      testGenerate(4, 4);
+
+      // Should throw
+      // testGenerate(4, 0);
     }
 };
 
