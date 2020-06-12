d64cc7776095ae400fa2d5dec73da50c1bb14d67
erobert <etiennerobert33@gmail.com>
2020-06-12 15:54:53 +0200

    use curly brackets initializations

--- generateXOString.cpp
+++ generateXOString.cpp
@@ -14,10 +14,10 @@ string generateXOString(unsigned int countX, unsigned int countO)
   ostringstream oss;
   oss.exceptions(ostringstream::failbit | ostringstream::badbit);
 
-  unsigned int counts[2] = { [X]=countX, [O]=countO };
+  unsigned int counts[2] { [X]=countX, [O]=countO };
 
-  Char lastChar = X;
-  unsigned int lastCharCount = 0;
+  Char lastChar { X };
+  unsigned int lastCharCount { 0 };
 
   const auto addChar = [&oss, &counts, &lastChar, &lastCharCount](Char c) {
     --counts[c];
@@ -35,12 +35,12 @@ string generateXOString(unsigned int countX, unsigned int countO)
   while (counts[X] > 0 || counts[O] > 0)
   {
     // We will attempt to add the Char that still has the highest count
-    const Char highestCountChar = counts[X] > counts[O] ? X : O;
+    const Char highestCountChar { counts[X] > counts[O] ? X : O };
     // Except if the last 3 characters were the highest count char
     // in which case we try to add the lowest count char
     if (lastChar == highestCountChar && lastCharCount == 3)
     {
-      const Char lowestCountChar = highestCountChar == X ? O : X;
+      const Char lowestCountChar { highestCountChar == X ? O : X };
       if (counts[lowestCountChar] == 0)
         throw invalid_argument("Cannot generate XO string with the given values");
       addChar(lowestCountChar);
